8987e19fb75d0e09109086c81bf0040d65e22781
李顺翔 <lishunxiang@boshtc.com>
2019-02-19 23:02:37 +0800

    添加分布式配置中心客户端案例

--- springcloud-config-client/src/main/java/com/sxli/springcloud/config/ConfigClientApplication.java
+++ springcloud-config-client/src/main/java/com/sxli/springcloud/config/ConfigClientApplication.java
@@ -2,14 +2,12 @@ package com.sxli.springcloud.config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigApplication {
+public class ConfigClientApplication {
     public static void main(String[] args) throws Exception {
-        new SpringApplicationBuilder(ConfigApplication.class).web(true).run(args);
+        new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
     }
 }
---
+++ springcloud-config-server/src/main/java/com/sxli/springcloud/config/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.sxli.springcloud.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+    public static void main(String[] args) throws Exception {
+        new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+    }
+}
